f445dc8800ec573b049704ca98228b0bf35299d2
Luiz Gustavo De Almeida Lopes <gustavw1001@gmail.com>
2026-02-24 16:06:32 -0300

    📃 TogglePage script + Styles
    - Styles fix
    - Toggle page function

--- public/JS/script.js
+++ public/JS/script.js
@@ -1,26 +1,40 @@
 const body = document.body
 
-
 const suspMenuIcon  = document.querySelectorAll(".suspended-menu .icon-bg")
 const suspMenuBg    = document.querySelector(".suspended-menu .selected-bg")
 
-let lastLabel = null
+const nfTaskIcon = `
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-label="nf-tasks-icon">
+        <path d="M12.6761 19.9589C12.9508 20.0228 12.976 20.3827 12.7084 20.4719L11.1284 20.9919C7.15839 22.2719 5.06839 21.2019 3.77839 17.2319L2.49839 13.2819C1.21839 9.31187 2.27839 7.21187 6.24839 5.93187L6.77238 5.75834C7.17525 5.62493 7.56731 6.02899 7.45292 6.43766C7.39622 6.64023 7.34167 6.85164 7.28839 7.07188L6.30839 11.2619C5.20839 15.9719 6.81839 18.5719 11.5284 19.6919L12.6761 19.9589Z" fill="currentColor"/>
+        <path d="M17.1702 3.20854L15.5002 2.81854C12.1602 2.02854 10.1702 2.67854 9.00018 5.09854C8.70018 5.70854 8.46018 6.44854 8.26018 7.29854L7.28018 11.4885C6.30018 15.6685 7.59018 17.7285 11.7602 18.7185L13.4402 19.1185C14.0202 19.2585 14.5602 19.3485 15.0602 19.3885C18.1802 19.6885 19.8402 18.2285 20.6802 14.6185L21.6602 10.4385C22.6402 6.25854 21.3602 4.18854 17.1702 3.20854ZM15.2902 13.3285C15.2002 13.6685 14.9002 13.8885 14.5602 13.8885C14.5002 13.8885 14.4402 13.8785 14.3702 13.8685L11.4602 13.1285C11.0602 13.0285 10.8202 12.6185 10.9202 12.2185C11.0202 11.8185 11.4302 11.5785 11.8302 11.6785L14.7402 12.4185C15.1502 12.5185 15.3902 12.9285 15.2902 13.3285ZM18.2202 9.94854C18.1302 10.2885 17.8302 10.5085 17.4902 10.5085C17.4302 10.5085 17.3702 10.4985 17.3002 10.4885L12.4502 9.25854C12.0502 9.15854 11.8102 8.74854 11.9102 8.34854C12.0102 7.94854 12.4202 7.70854 12.8202 7.80854L17.6702 9.03854C18.0802 9.12854 18.3202 9.53854 18.2202 9.94854Z" fill="currentColor"/>
+    </svg>
+`
 
-suspMenuIcon.forEach(icon => {
-    if(icon.classList.contains("selected")){
-        suspMenuBg.dataset.id = icon.dataset.id
-        suspMenuBg.dataset.id = lastLabel
-    }
+const fTaskIcon = `
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-label="finished-tasks-icon">
+        <g>
+            <path d="M17.5821 6.95711C17.9726 6.56658 17.9726 5.93342 17.5821 5.54289C17.1916 5.15237 16.5584 5.15237 16.1679 5.54289L5.54545 16.1653L1.70711 12.327C1.31658 11.9365 0.683417 11.9365 0.292893 12.327C-0.0976311 12.7175 -0.097631 13.3507 0.292893 13.7412L4.83835 18.2866C5.22887 18.6772 5.86204 18.6772 6.25256 18.2866L17.5821 6.95711Z" fill="currentColor"/>
+            <path d="M23.5821 6.95711C23.9726 6.56658 23.9726 5.93342 23.5821 5.54289C23.1915 5.15237 22.5584 5.15237 22.1678 5.54289L10.8383 16.8724C10.4478 17.263 10.4478 17.8961 10.8383 18.2866C11.2288 18.6772 11.862 18.6772 12.2525 18.2866L23.5821 6.95711Z" fill="currentColor"/>
+        </g>
+        <defs>
+            <clipPath>
+                <rect width="24" height="24" fill="none"/>
+            </clipPath>
+        </defs>
+    </svg>
+`
 
-    icon.addEventListener("mouseover", () => {
-        suspMenuBg.dataset.id = icon.dataset.id
-    })
+const emptySquare = `
+    <svg class="empty" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-label="square-icon">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M4.5 2C3.11929 2 2 3.11929 2 4.5V19.5C2 20.8807 3.11929 22 4.5 22H19.5C20.8807 22 22 20.8807 22 19.5V4.5C22 3.11929 20.8807 2 19.5 2H4.5ZM4 4.5C4 4.22386 4.22386 4 4.5 4H19.5C19.7761 4 20 4.22386 20 4.5V19.5C20 19.7761 19.7761 20 19.5 20H4.5C4.22386 20 4 19.7761 4 19.5V4.5Z" fill="currentColor"/>
+    </svg>
+`
 
-    icon.addEventListener("mouseleave", () => {
-        suspMenuBg.dataset.id = lastLabel
-    })
-})
-
+const fullSquare = `
+    svg class="full" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M2 4.5C2 3.11929 3.11929 2 4.5 2H19.5C20.8807 2 22 3.11929 22 4.5V19.5C22 20.8807 20.8807 22 19.5 22H4.5C3.11929 22 2 20.8807 2 19.5V4.5ZM18.787 9.57537C19.1767 9.18401 19.1753 8.55084 18.784 8.16116L18.0753 7.45558C17.684 7.0659 17.0508 7.06726 16.6611 7.45863L10.8895 13.2551L7.56845 9.98027C7.1752 9.59249 6.54205 9.59692 6.15427 9.99018L5.45213 10.7022C5.06436 11.0955 5.06879 11.7286 5.46204 12.1164L10.2003 16.7888C10.5922 17.1752 11.2228 17.1723 11.6111 16.7823L18.787 9.57537Z" fill="currentColor"/>
+    </svg>
+`
 
 /* TOGGLE THEME (dark mode) */
 const toggleThemeIcon = document.querySelector(".toggle-theme")
@@ -41,6 +55,69 @@ function toggleTheme(){
 /* LOCAL STORAGE */
 const lastTheme = localStorage.getItem("theme") || "light"
 if(lastTheme == "dark"){toggleTheme()}
+
+
+let currentPage = localStorage.getItem("lastPage") || "1"
+togglePage(currentPage);
+suspMenuBg.dataset.id = currentPage;
+
+suspMenuIcon.forEach(icon =>{
+    icon.addEventListener("click", () => {
+        const id = icon.dataset.id
+        currentPage = id
+        togglePage(id)
+
+        localStorage.setItem("lastPage", id)
+        suspMenuBg.dataset.id = id
+    })
+
+    icon.addEventListener("mouseover", () => {
+        suspMenuBg.dataset.id = icon.dataset.id
+    })
+
+    icon.addEventListener("mouseleave", () => {
+        suspMenuBg.dataset.id = currentPage
+    })
+})
+
+
+function togglePage(index){
+    const headerTitle   = document.querySelector("header .title")
+    const headerIcon    = headerTitle.querySelector(".icon")
+    const headerText    = headerTitle.querySelector(".title-text")
+
+    const addTasksIcon = document.querySelector(".add-tasks")
+
+    switch(index){
+        case "1":
+            headerIcon.innerHTML = nfTaskIcon
+            headerText.innerHTML = `
+                <h1>Not Finished Tasks</h1>
+                <p>Click on a task to edit. Click on the + button to add tasks.</p>
+            `
+            addTasksIcon.classList.remove("inactive")
+            break;
+        default:
+            headerIcon.innerHTML = fTaskIcon
+            headerText.innerHTML = `
+                <h1> Finished Tasks</h1>
+                <p>Click on a task to edit. Click on the + button to add tasks.</p>
+            `
+            addTasksIcon.classList.add("inactive")
+
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
