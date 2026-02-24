9b6e6a7d0cd3411e111a91893cd264ee8aa83907
Luiz Gustavo De Almeida Lopes <gustavw1001@gmail.com>
2026-02-24 17:36:23 -0300

    🟦 Popup styles + script

--- public/JS/script.js
+++ public/JS/script.js
@@ -3,6 +3,12 @@ const body = document.body
 const suspMenuIcon  = document.querySelectorAll(".suspended-menu .icon-bg")
 const suspMenuBg    = document.querySelector(".suspended-menu .selected-bg")
 
+const xIcon = `
+    <svg class="close-icon" onclick="closePopUp()" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-label="x-icon">
+        <path d="M20.7457 3.32851C20.3552 2.93798 19.722 2.93798 19.3315 3.32851L12.0371 10.6229L4.74275 3.32851C4.35223 2.93798 3.71906 2.93798 3.32854 3.32851C2.93801 3.71903 2.93801 4.3522 3.32854 4.74272L10.6229 12.0371L3.32856 19.3314C2.93803 19.722 2.93803 20.3551 3.32856 20.7457C3.71908 21.1362 4.35225 21.1362 4.74277 20.7457L12.0371 13.4513L19.3315 20.7457C19.722 21.1362 20.3552 21.1362 20.7457 20.7457C21.1362 20.3551 21.1362 19.722 20.7457 19.3315L13.4513 12.0371L20.7457 4.74272C21.1362 4.3522 21.1362 3.71903 20.7457 3.32851Z" fill="currentColor"/>
+    </svg>
+`
+
 const nfTaskIcon = `
     <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-label="nf-tasks-icon">
         <path d="M12.6761 19.9589C12.9508 20.0228 12.976 20.3827 12.7084 20.4719L11.1284 20.9919C7.15839 22.2719 5.06839 21.2019 3.77839 17.2319L2.49839 13.2819C1.21839 9.31187 2.27839 7.21187 6.24839 5.93187L6.77238 5.75834C7.17525 5.62493 7.56731 6.02899 7.45292 6.43766C7.39622 6.64023 7.34167 6.85164 7.28839 7.07188L6.30839 11.2619C5.20839 15.9719 6.81839 18.5719 11.5284 19.6919L12.6761 19.9589Z" fill="currentColor"/>
@@ -111,12 +117,71 @@ function togglePage(index){
 
 
 
+function closePopUp(){
+    const closeIcon = document.querySelectorAll(".close-icon")
+    closeIcon.forEach(icon => {
+        let closestPopUp = icon.closest(".popup-bg")
+        if(closestPopUp.classList.contains("active")){
+            closestPopUp.classList.add("fade-out")
+
+            setTimeout(() => {
+                closestPopUp.classList.remove("active")
+                closestPopUp.classList.remove("fade-out")
+            }, 300)
+        }
+    })
+}
+
+/*fillPopUp("edit", {
+    Title: "Comprar pão",
+    Description: "Passar na padaria depois do trabalho"
+})
 
 
+*/
+
+function fillPopUp(type, values = null){
+    const popupBg       = document.querySelector(".popup-bg")
+    const popup         = popupBg.querySelector(".popup")
+
+    const popupTitle    = popup.querySelector(".title")
+    const popupForm     = popup.querySelector("form")
+    const submitBtn   = popup.querySelector(".submit")
+    const removeTaskBtn = popup.querySelector(".remove-task")
+
+    popupBg.classList.add("active")
+
+    // clear form
+    popupForm.innerHTML = ""
+    
+    switch(type){
+        case "add": 
+            popupTitle.innerHTML = `<h1>Add Task</h1> ${xIcon}`
+            popupForm.innerHTML = `
+                <input class="regular-input" type="text" name="title" id="ititle" placeholder="Title here">
+                <input class="regular-input" type="text" name="desc" id="idesc" placeholder="Description here (150 characters)">
+            `
+            submitBtn.innerHTML = `Add`
+            removeTaskBtn.classList.add("inactive")
+            break;
+        case "edit":
+            popupTitle.innerHTML = `<h1>Modify Task</h1> ${xIcon}`
+
+            if(values != null){
+                for (const [key, value] of Object.entries(values)){
+                    popupForm.innerHTML += `<input class="regular-input" type="text" name="${value}" id="i${value}" placeholder="${key}" value="${value}">`
+                }
+            }
+
+            submitBtn.innerHTML = `Save Changes`
+            break;
+    }
+}
 
 
-
-
+function openPopup(type){
+    alert(type)
+}
 
 
 
